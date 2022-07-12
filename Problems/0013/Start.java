2bfcc6493330348fcb6f179f62833d1891d5a230
Bluemangoo <chenfy2006@qq.com>
2022-07-12 16:00:39 +0800

    20220712

--- Problems/0013/Start.java
+++ Problems/0013/Start.java
@@ -1,35 +1,35 @@
-//Problem 0020
+//Problem 0013
 public class Start {
     private static void test(int count) {
         switch (count) {
             case 1: {
                 System.out.println("test 1 start:");
-                System.out.println(new Solution().isValid("()"));
-                System.out.println("Should be: true\n");
+                System.out.println(new Solution().romanToInt("III"));
+                System.out.println("Should be: 3\n");
                 break;
             }
             case 2: {
                 System.out.println("test 2 start:");
-                System.out.println(new Solution().isValid("()[]{}"));
-                System.out.println("Should be: true\n");
+                System.out.println(new Solution().romanToInt("IV"));
+                System.out.println("Should be: 4\n");
                 break;
             }
             case 3: {
                 System.out.println("test 3 start:");
-                System.out.println(new Solution().isValid("(]"));
-                System.out.println("Should be: false\n");
+                System.out.println(new Solution().romanToInt("IX"));
+                System.out.println("Should be: 9\n");
                 break;
             }
             case 4: {
                 System.out.println("test 4 start:");
-                System.out.println(new Solution().isValid("([)]"));
-                System.out.println("Should be: false\n");
+                System.out.println(new Solution().romanToInt("LVIII"));
+                System.out.println("Should be: 58\n");
                 break;
             }
             case 5: {
                 System.out.println("test 5 start:");
-                System.out.println(new Solution().isValid("{[]}"));
-                System.out.println("Should be: true\n");
+                System.out.println(new Solution().romanToInt("MCMXCIV"));
+                System.out.println("Should be: 1994\n");
                 break;
             }
             default: {
