ae076292c40f9ea21dc31722ac0514ba137e336e
Bluemangoo <chenfy2006@qq.com>
2022-07-12 15:09:42 +0800

    20220712

---
+++ Problems/0014/Solution.java
@@ -0,0 +1,59 @@
+//编写一个函数来查找字符串数组中的最长公共前缀。
+//
+// 如果不存在公共前缀，返回空字符串 ""。
+//
+//
+//
+// 示例 1：
+//
+//
+//输入：strs = ["flower","flow","flight"]
+//输出："fl"
+//
+//
+// 示例 2：
+//
+//
+//输入：strs = ["dog","racecar","car"]
+//输出：""
+//解释：输入不存在公共前缀。
+//
+//
+//
+// 提示：
+//
+//
+// 1 <= strs.length <= 200
+// 0 <= strs[i].length <= 200
+// strs[i] 仅由小写英文字母组成
+//
+//
+// Related Topics 字符串 👍 2332 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        String common = strs[0];
+        if (strs.length == 1) {
+            return strs[0];
+        }
+        for (String string : strs) {
+            for (int i = 0; i < common.length(); i++) {
+                if (string.length()<i+1){
+                    common = common.substring(0, i);
+                    break;
+                }
+                if (common.charAt(i) != string.charAt(i)) {
+                    common = common.substring(0, i);
+                    break;
+                }
+            }
+            if (common.length() == 0) {
+                break;
+            }
+        }
+        return common;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
---
+++ Problems/0014/Start.java
@@ -0,0 +1,44 @@
+//Problem 0014
+public class Start {
+    private static void test(int count){
+        switch (count) {
+            case 1:{
+                System.out.println("test 1 start:");
+                String[] strsIn={"flower","flow","flight"};
+                System.out.println(new Solution().longestCommonPrefix(strsIn));
+                System.out.println("Should be: fl\n");
+                break;
+            }
+            case 2:{
+                System.out.println("test 2 start:");
+                String[] strsIn={"dog","racecar","car"};
+                System.out.println(new Solution().longestCommonPrefix(strsIn));
+                System.out.println("Should be: \n");
+                break;
+            }
+            case 3:{
+                System.out.println("test 3 start:");
+                String[] strsIn={"ab","a"};
+                System.out.println(new Solution().longestCommonPrefix(strsIn));
+                System.out.println("Should be: a\n");
+                break;
+            }
+            case 4:{
+                System.out.println("test 4 start:");
+                String[] strsIn={"","a"};
+                System.out.println(new Solution().longestCommonPrefix(strsIn));
+                System.out.println("Should be: \n");
+                break;
+            }
+            default:{
+                System.err.println("Too More Tests");
+            }
+        }
+    }
+    public static void main(String[] args) {
+        int testTime = 4;
+        for (int i=1;i<=testTime;i++){
+            test(i);
+        }
+    }
+}
