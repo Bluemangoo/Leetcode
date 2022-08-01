540541b4bb389738c9cbcccf3718442c62b2068a
Bluemangoo <chenfy2006@qq.com>
2022-08-01 09:29:56 +0800

    20220801

---
+++ Problems/1/3/7/1374/Start.java
@@ -0,0 +1,36 @@
+//Problem 1374
+public class Start {
+    private static void test(int count) {
+        switch (count) {
+            case 1: {
+                System.out.println("test "+count+" start:");
+                System.out.println(new Solution().generateTheString(4));
+                System.out.println(" -  <-  Should be\n");
+                break;
+            }
+            case 2: {
+                System.out.println("test "+count+" start:");
+                System.out.println(new Solution().generateTheString(2));
+                System.out.println(" -  <-  Should be\n");
+                break;
+            }
+            case 3: {
+                System.out.println("test "+count+" start:");
+                System.out.println(new Solution().generateTheString(7));
+                System.out.println(" -  <-  Should be\n");
+                break;
+            }
+            default: {
+                System.err.println("Too More Tests");
+                System.exit(-1);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int testTime = 3;
+        for (int i = 1; i <= testTime; i++) {
+            test(i);
+        }
+    }
+}
