6f37be4a69cdd639d3b448c5a7c11ec7dd5f7c14
Bluemangoo <chenfy2006@qq.com>
2022-08-02 09:39:27 +0800

    20220802

---
+++ Problems/0/6/2/0622/Start.java
@@ -0,0 +1,52 @@
+//Problem 0622
+public class Start {
+    private static void test(int count) {
+        switch (count) {
+            case 1: {
+                MyCircularQueue circularQueue = new MyCircularQueue(3);
+                System.out.println("test "+count+" start:");
+                System.out.print("["+circularQueue.enQueue(1)+", ");
+                System.out.print(circularQueue.enQueue(2)+", ");
+                System.out.print(circularQueue.enQueue(3)+", ");
+                System.out.print(circularQueue.enQueue(4)+", ");
+                System.out.print(circularQueue.Rear()+", ");
+                System.out.print(circularQueue.isFull()+", ");
+                System.out.print(circularQueue.deQueue()+", ");
+                System.out.print(circularQueue.enQueue(4)+", ");
+                System.out.print(circularQueue.Rear()+", ");
+                System.out.print(circularQueue.isEmpty()+", ");
+                System.out.println(circularQueue.Front()+"]");
+                System.out.println("[true, true, true, false, 3, true, true, true, 4, false, 2]  <-  Should be\n");
+                break;
+            }
+            case 2: {
+                System.out.println("test "+count+" start:");
+                MyCircularQueue circularQueue = new MyCircularQueue(8);
+                System.out.print("["+circularQueue.enQueue(3)+", ");
+                System.out.print(circularQueue.enQueue(9)+", ");
+                System.out.print(circularQueue.enQueue(5)+", ");
+                System.out.print(circularQueue.enQueue(0)+", ");
+                System.out.print(circularQueue.deQueue()+", ");
+                System.out.print(circularQueue.deQueue()+", ");
+                System.out.print(circularQueue.isEmpty()+", ");
+                System.out.print(circularQueue.isEmpty()+", ");
+                System.out.print(circularQueue.Rear()+", ");
+                System.out.print(circularQueue.Rear()+", ");
+                System.out.println(circularQueue.deQueue()+"]");
+                System.out.println("[true, true, true, true, true, true, false, false, 0, 0, true]  <-  Should be\n");
+                break;
+            }
+            default: {
+                System.err.println("Too More Tests");
+                System.exit(-1);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int testTime = 2;
+        for (int i = 1; i <= testTime; i++) {
+            test(i);
+        }
+    }
+}
