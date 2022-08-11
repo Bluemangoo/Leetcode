4cbcc7a610de32d2b1d48d53b2f53adbe79c0e8f
Bluemangoo <chenfy2006@qq.com>
2022-08-11 16:19:44 +0800

    20220811

---
+++ Problems/1/4/1/1417/Start.java
@@ -0,0 +1,48 @@
+//Problem 1417
+public class Start {
+    private static void test(int count) {
+        switch (count) {
+            case 1: {
+                System.out.println("test "+count+" start:");
+                System.out.println(new Solution().reformat("a0b1c2"));
+                System.out.println("0a1b2c  <-  Should be\n");
+                break;
+            }
+            case 2: {
+                System.out.println("test "+count+" start:");
+                System.out.println(new Solution().reformat("leetcode"));
+                System.out.println("  <-  Should be\n");
+                break;
+            }
+            case 3: {
+                System.out.println("test "+count+" start:");
+                System.out.println(new Solution().reformat("1229857369"));
+                System.out.println("  <-  Should be\n");
+                break;
+            }
+            case 4: {
+                System.out.println("test "+count+" start:");
+                System.out.println(new Solution().reformat("covid2019"));
+                System.out.println("c2o0v1i9d  <-  Should be\n");
+                break;
+            }
+            case 5: {
+                System.out.println("test "+count+" start:");
+                System.out.println(new Solution().reformat("ab123"));
+                System.out.println("1a2b3  <-  Should be\n");
+                break;
+            }
+            default: {
+                System.err.println("Too More Tests");
+                System.exit(-1);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int testTime = 5;
+        for (int i = 1; i <= testTime; i++) {
+            test(i);
+        }
+    }
+}
